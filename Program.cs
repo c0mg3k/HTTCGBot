30539d5fdee02e95ca49fa2e870c805e21d69886
Josh Christiansen <jchristiansen@LoanPaymentPro.tech>
2021-10-28 15:48:43 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace HTTCGDiscordBot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+    }
+}
---
+++ Services/CommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTCGDiscordBot.Services
+{
+    public class CommandHandler
+    {
+        public static IServiceProvider _provider;
+        public readonly DiscordSocketClient _discord;
+        public readonly CommandService _commands;
+        public readonly IConfigurationRoot _config;
+        public CommandHandler(DiscordSocketClient discord, CommandService commands, IConfigurationRoot config, IServiceProvider provider)
+        {
+            _provider = provider;
+            _discord = discord;
+            _commands = commands;
+            _config = config;
+
+            _discord.Ready += OnReady;
+        }
+
+        private Task OnReady()
+        {
+            Console.WriteLine($"Connected as {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
+            return Task.CompletedTask;
+        }
+    }
+}
